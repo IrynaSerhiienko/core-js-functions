50c184cb532474f189938d68428b2730e3573353
Iryna Serhiienko <irishka.sergienko@gmail.com>
2025-03-25 01:25:27 +0200

    feat: add tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -77,8 +77,10 @@ function getArgumentsCount(funcs) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return function innerFunction(x) {
+    return x ** exponent;
+  };
 }
 
 /**
@@ -196,8 +198,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currentId = startFrom;
+  return function idGenerator() {
+    const result = currentId;
+    currentId += 1;
+    return result;
+  };
 }
 
 module.exports = {
